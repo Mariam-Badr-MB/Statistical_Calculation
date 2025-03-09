e0f5739af7aea326c4baa8315249e794f8e0456a
Johnayman1 <160542216+Johnayman1@users.noreply.github.com>
2025-03-09 16:55:45 +0200

    Added Merge Sort

--- Statistical Calculation.cpp
+++ Statistical Calculation.cpp
@@ -46,19 +46,57 @@ public:
 };
 
 // ************************** Implement Sort Algorithm  **************************
+template <class T>
+void merge(T data[] ,int l ,int m ,int r) {
+    int i , j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    T L[n1], R[n2];
+    for (i = 0; i < n1; i++) {
+        L[i] = data [l + i];
+    }
+    for (j = 0; j < n2; j++) {
+        R[j] = data [m + j + 1];
+    }
+    i = j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            data [k] = L[i];
+            i++;
+        }else {
+            data [k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) {
+        data [k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        data [k] = R[j];
+        j++;
+        k++;
+    }
 
+}
+
+template <class T>
+void mergeSort(T data[] , int l , int r) {
+    if (l < r) {
+        int mid = l + (r - l) / 2;
+        mergeSort(data, l, mid);
+        mergeSort(data, mid+1, r);
+        merge(data, l, mid, r);
+    }
+}
+// ************************** Implement Sort Function  **************************
 template<typename T>
 void StatisticalCalculation<T>::sort() {
-// Using Bubble Sort Algorithm
-    for (int i = 0; i < this->size - 1; ++i) {
-        for (int j = 0; j < this->size - i - 1; ++j) {
-            if (data[j] > data[j + 1]) {
-                T temp = data[j];
-                data[j] = data[j + 1];
-                data[j + 1] = temp;
-            }
-        }
-    }
+    // Using merge Sort Algorithm
+    mergeSort(data, 0, size-1);
 }
 
 // ************************** Implement Find Median  **************************
@@ -200,5 +238,4 @@ int main() {
     statistics.statisticsMenu();
     return 0;
 }
-
 
